4bb915000f84025b207371c8e848d02e4d621f0e
pthienquan1 <quan01011997123@gmail.com>
2023-05-30 21:47:31 +0700

    extracts claims from jwt & implement the getSignInKey method

--- src/main/java/com/pthienquan1/security/service/JwtService.java
+++ src/main/java/com/pthienquan1/security/service/JwtService.java
@@ -1,6 +1,12 @@
 package com.pthienquan1.security.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 /**
  * Created by quan0
@@ -9,7 +15,23 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "4A614E645266556A586E3272357538782F413F4428472B4B6250655368566B59";
     public String extractUsername(String jwt) {
         return null;
+    }
+    public Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
