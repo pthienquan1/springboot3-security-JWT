c4267c12731bf2464de8bceeb3c62b0837c91c28
pthienquan1 <quan01011997123@gmail.com>
2023-05-31 14:00:40 +0700

    implement register method

--- src/main/java/com/pthienquan1/security/service/AuthenticationService.java
+++ src/main/java/com/pthienquan1/security/service/AuthenticationService.java
@@ -3,7 +3,11 @@ package com.pthienquan1.security.service;
 import com.pthienquan1.security.auth.AuthenticationRequest;
 import com.pthienquan1.security.auth.AuthenticationResponse;
 import com.pthienquan1.security.auth.RegisterRequest;
+import com.pthienquan1.security.entity.Role;
+import com.pthienquan1.security.entity.User;
+import com.pthienquan1.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,8 +18,24 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
-    public AuthenticationResponse register(RegisterRequest request) {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    private final JwtService jwtService;
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+        
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
